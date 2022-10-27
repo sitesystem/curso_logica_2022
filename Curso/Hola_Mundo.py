671d826f707cd85e7769b967c1d3eca7a5e45f05
sitesystem <l_aloaxa05@hotmail.com>
2022-10-27 18:53:16 -0500

    1ra subida de archivo de Python

---
+++ Curso/Hola_Mundo.py
@@ -0,0 +1,9 @@
+# EJEMPLO 1. Saludo
+# COMENTARIOS: Sirven para documentar o detallar cada parte del programa.
+# Los comentarios los omite o no los toma en cuenta Python
+
+# Imprimiendo o mostrando en pantalla mensajes o resultados
+print("Hola Mundo")
+print('Eduardo');
+print(10+5)
+print(9*8)
