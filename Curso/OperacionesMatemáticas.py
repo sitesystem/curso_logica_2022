c74f8d5df19b3f4ac53b2faea2f4335bd8376a40
sitesystem <l_aloaxa05@hotmail.com>
2022-10-27 20:03:31 -0500

    Oper Matem

---
+++ Curso/OperacionesMatemáticas.py
@@ -0,0 +1,24 @@
+# Ejemplo 2. Operaciones Matemáticas
+
+# ENTRADAS DE DATOS
+# Declarar o crear las variables
+número_1 = 10
+número_2 = 2
+# Declarar o crear constantes: Elemento que su valor permanece fijo
+PI = 3.1416
+
+
+# PROCESOS (Cálculos u Operaciones Matemáticas y/o Lógicas: if, elif y else)
+suma = número_1 + número_2
+resta = número_1 - número_2
+
+
+
+# SALIDAS DE DATOS (Mostrar los resultados)
+print("La suma es =", suma)
+print("La suma es = " + str(suma)) # CONCATENACIÓN: Unión de dos o más textos
+# Conversión de un tipo de dato en otro tipo de dato (CASTEO)
+print(f"La suma es = {suma}") # Interpolación de textos f: formateo
+
+print(resta)
+
